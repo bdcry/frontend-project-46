eef844f2719a010e7903b1abe4163a60b9f02d15
Tim <tim@MacBook-Pro-2.local>
2024-03-04 19:47:02 +0300

    made the second step of project. added -f

--- gendiff.js
+++ gendiff.js
@@ -2,8 +2,10 @@
 import { program } from 'commander';
 
 program
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
