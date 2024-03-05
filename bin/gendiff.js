80448c69f130833cc340ec9bab843d3d230f7e91
Tim <tim@MacBook-Pro-2.local>
2024-03-06 01:56:36 +0300

    update gendiff, add parsefile

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import parseFile from '../src/parser/jsonParser.js';
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const data1 = parseFile(filepath1);
+    const data2 = parseFile(filepath2);
+    console.log('Data from filepath1:', data1);
+    console.log('Data from filepath2:', data2);
+  });
 
 program.parse();
--- src/parser/jsonParser.js
+++ src/parser/jsonParser.js
@@ -1,18 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-// read json files
-const dirName = process.cwd();
+const parseFile = (filePath) => {
+  const dirName = process.cwd();
+  const data = fs.readFileSync(path.resolve(dirName, '__fixtures__', filePath));
+  const dataObj = JSON.parse(data);
+  return dataObj;
+};
 
-const fullPath1 = path.resolve(dirName, '__fixtures__', 'file1.json');
-const data1 = fs.readFileSync(fullPath1);
-
-const fullPath2 = path.resolve(dirName, '__fixtures__', 'file2.json');
-const data2 = fs.readFileSync(fullPath2);
-
-// create object
-const dataObj1 = JSON.parse(data1);
-console.log(dataObj1);
-
-const dataObj2 = JSON.parse(data2);
-console.log(dataObj2);
+export default parseFile;
