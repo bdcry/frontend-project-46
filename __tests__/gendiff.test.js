74a9151820d6e654d4f9237e7b7e892059bdbc5d
bdcry <tim040404@mail.ru>
2024-03-15 01:19:22 +0300

    added support for nested structures v1.7.0

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import gendiff from '../src/gendiff.js';
@@ -7,110 +8,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '__tests-fixtures__', filename);
+const readContent = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const expectedJson = readContent('expected.txt');
 
 // JSON format check
-test('checking on empty files JSON', () => {
-  const expectedDiffEmptyFiles = `{
-
-}`;
-  const filePath1 = getFixturePath('emptyfile1.json');
-  const filePath2 = getFixturePath('emptyfile2.json');
-  expect(gendiff(filePath1, filePath2)).toEqual(expectedDiffEmptyFiles);
-});
-
-test('checking on same files JSON', () => {
-  const expectedDiffSameFiles = `{
-  follow: false
-  host: hexlet.io
-  proxy: 123.234.53.22
-  timeout: 50
-}`;
-  const filePath1 = getFixturePath('file1.json');
-  const filePath2 = getFixturePath('file1.json');
-  expect(gendiff(filePath1, filePath2)).toEqual(expectedDiffSameFiles);
-});
-
-test('check for deletion in a file JSON', () => {
-  const expectedDiffDelSmthInFile = `{
-  - follow: false
-  host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+test('it should correctly compare two JSON files with nested structures', () => {
   const filePath1 = getFixturePath('file1.json');
   const filePath2 = getFixturePath('file2.json');
-  expect(gendiff(filePath1, filePath2)).toEqual(expectedDiffDelSmthInFile);
-});
 
-// YAML format check
-test('checking on empty files YAML', () => {
-  const expectedDiffEmptyFiles = `{
+  const diff = gendiff(filePath1, filePath2);
 
-}`;
-  const filePath1 = getFixturePath('emptyfile1.yml');
-  const filePath2 = getFixturePath('emptyfile2.yml');
-  expect(gendiff(filePath1, filePath2)).toEqual(expectedDiffEmptyFiles);
-});
-
-test('checking on same files YAML (yml)', () => {
-  const expectedDiffSameFiles = `{
-  follow: false
-  host: hexlet.io
-  proxy: 123.234.53.22
-  timeout: 50
-}`;
-  const filePath1 = getFixturePath('file1.yml');
-  const filePath2 = getFixturePath('file1.yml');
-  expect(gendiff(filePath1, filePath2)).toEqual(expectedDiffSameFiles);
-});
-
-test('check for deletion in a file YAML (yml)', () => {
-  const expectedDiffDelSmthInFile = `{
-  - follow: false
-  host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
-  const filePath1 = getFixturePath('file1.yml');
-  const filePath2 = getFixturePath('file2.yml');
-  expect(gendiff(filePath1, filePath2)).toEqual(expectedDiffDelSmthInFile);
-});
-
-test('checking on same files YAML (yaml)', () => {
-  const expectedDiffSameFiles = `{
-  follow: false
-  host: hexlet.io
-  proxy: 123.234.53.22
-  timeout: 50
-}`;
-  const filePath1 = getFixturePath('file1.yaml');
-  const filePath2 = getFixturePath('file1.yaml');
-  expect(gendiff(filePath1, filePath2)).toEqual(expectedDiffSameFiles);
-});
-
-test('check for deletion in a file YAML (yaml)', () => {
-  const expectedDiffDelSmthInFile = `{
-  - follow: false
-  host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
-  const filePath1 = getFixturePath('file1.yaml');
-  const filePath2 = getFixturePath('file2.yaml');
-  expect(gendiff(filePath1, filePath2)).toEqual(expectedDiffDelSmthInFile);
-});
-
-test('checking on wrong format', () => {
-  const filePath1 = getFixturePath('file1.jsons');
-  const filePath2 = getFixturePath('file2.yaml');
-  // Используем стрелочную функцию для того, чтобы
-  // обернуть вызов gendiff, позволив jest получить исключение,
-  // а не результат.
-  expect(() => gendiff(filePath1, filePath2)).toThrow();
+  expect(diff).toEqual(expectedJson);
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,27 +1,43 @@
 import _ from 'lodash';
 import parseFile from './parser/parsers.js';
 
+const formatValue = (value, depth) => {
+  if (!_.isObject(value)) {
+    return value;
+  }
+  const indent = ' '.repeat(depth * 4);
+  const lines = Object.entries(value).map(([key, val]) => `${indent}    ${key}: ${formatValue(val, depth + 1)}`);
+  return `{\n${lines.join('\n')}\n${indent}}`;
+};
+
 const gendiff = (filePath1, filePath2) => {
   const obj1 = parseFile(filePath1);
   const obj2 = parseFile(filePath2);
 
-  // создаем и сортируем массив с уникальными парами ключей
-  const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-
   // создаем вложенную функцию поиска различий
-  const diffs = keys.map((key) => {
-    if (!Object.hasOwn(obj1, key)) {
-      return `  + ${key}: ${obj2[key]}`;
-    }
-    if (!Object.hasOwn(obj2, key)) {
-      return `  - ${key}: ${obj1[key]}`;
-    }
-    if (obj1[key] === obj2[key]) {
-      return `  ${key}: ${obj1[key]}`;
-    }
-    return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
-  });
-  return `{\n${diffs.join('\n')}\n}`;
+  const iter = (currentObj1, currentObj2, depth) => {
+    // создаем и сортируем массив с уникальными парами ключей
+    const keys = _.sortBy(_.union(Object.keys(currentObj1), Object.keys(currentObj2)));
+    const indent = ' '.repeat(depth * 4 - 2);
+    const diffs = keys.map((key) => {
+      if (!_.has(currentObj2, key)) {
+        return `${indent}- ${key}: ${formatValue(currentObj1[key], depth)}`;
+      }
+      if (!_.has(currentObj1, key)) {
+        return `${indent}+ ${key}: ${formatValue(currentObj2[key], depth)}`;
+      }
+      if (_.isObject(currentObj1[key]) && _.isObject(currentObj2[key])) {
+        return `${indent}  ${key}: {\n${iter(currentObj1[key], currentObj2[key], depth + 1)}\n${indent}  }`;
+      }
+      if (!_.isEqual(currentObj1[key], currentObj2[key])) {
+        return `${indent}- ${key}: ${formatValue(currentObj1[key], depth)}\n${indent}+ ${key}: ${formatValue(currentObj2[key], depth)}`;
+      }
+      return `${indent}  ${key}: ${formatValue(currentObj1[key], depth)}`;
+    });
+    return diffs.join('\n');
+  };
+
+  return `\n{\n${iter(obj1, obj2, 1)}\n}`;
 };
 
 export default gendiff;
