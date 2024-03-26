8fd4772f518264378a420e025a85533da6ccb6a4
bdcry <tim040404@mail.ru>
2024-03-26 21:34:18 +0300

    upd fixtures files v2.0.2

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,7 +7,7 @@ import gendiff from '../src/gendiff.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '__tests-fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readContent = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const expectedStylish = readContent('expected_stylish.txt');
